9ed3d91809607e903ec01ae131d2b7c90dd5845a
Ki <95108448+KSO2525@users.noreply.github.com>
2022-07-18 10:34:33 -0400

    Update checkpoints.py

--- checkpoints.py
+++ checkpoints.py
@@ -35,7 +35,7 @@ class Solution:
             if i > temp:
                 temp = i
         return temp
-    def bubblesort(arr):
+    def bubblesort(self, arr):
         for i in range(0, len(arr)):
             for j in range(0, len(arr)-1):
                 if arr[j] > arr[j+1]:
