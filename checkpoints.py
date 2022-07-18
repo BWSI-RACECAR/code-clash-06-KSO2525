82b36c063518038730fff3850cc45fe0dbdfedba
Ki <95108448+KSO2525@users.noreply.github.com>
2022-07-18 10:11:03 -0400

    Update checkpoints.py

--- checkpoints.py
+++ checkpoints.py
@@ -37,7 +37,6 @@ class Solution:
         # TODO: Write code below to return an int with the solution to the prompt
         for i in range(len(checkpoints) - 1):
             ranges.append(checkpoints[i+1] - checkpoints[i])
-        print(ranges)
         temp = ranges[0]
         for i in ranges:
             if i > temp:
